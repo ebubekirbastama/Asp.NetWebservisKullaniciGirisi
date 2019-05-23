d9e8799cb010396646e0e10f3f9046a606e128a4
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-05-23 07:28:52 +0300

    Asp.net Kullanıcı girisi
    Asp.net Kullanıcı girisi Web Servis

---
+++ Chat servis/Service1.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Chat_servis
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string KullanıcıGirisi(string d1, string s2)
+        {
+            var mesaj = "";
+            baglanti bgl = new baglanti();
+            SqlConnection con = bgl.Baglanti();
+            SqlCommand kmt1 = new SqlCommand("sp_k_giris", con);
+            kmt1.CommandType = CommandType.StoredProcedure;
+            kmt1.Parameters.AddWithValue("@k_adi", d1);
+            kmt1.Parameters.AddWithValue("@k_sifresi", s2);
+            int sonuc1 = kmt1.ExecuteNonQuery();
+            //MessageBox.Show(sonuc.ToString ());
+            if (sonuc1 == 0)
+            {
+                mesaj = "Kullanıcı Girişi Başarılı";
+            }
+            else if (sonuc1 == 1)
+            {
+                mesaj = "Kullanıcı Girişi Başarısız";
+            }
+            return mesaj;
+        }
+    }
+}
---
+++ Chat servis/baglanti.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace Chat_servis
+{
+    public class baglanti
+    {
+        public SqlConnection Baglanti()
+        {
+            SqlConnection con = new SqlConnection("Server=AZRAIL-PC; Integrated Security=true; Database=Online_Chat_Programı");
+            if (con.State == ConnectionState.Closed )
+            {
+                con.Open();
+            }
+            return con;
+
+        }
+    }
+}
